32177619b914cc61074fd7a267ee594e130894d5
Dyego Turl <dyegoturl@actionplus.com.br>
2020-05-16 17:32:37 +0400

    :rocket: final Challenge Finalized

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
-
-// const transactionsRepository = new TransactionsRepository();
+const transactionRepository = new TransactionRepository();
 
 transactionRouter.get('/', (request, response) => {
+  const transactions = transactionRepository.all();
+  const balance = transactionRepository.getBalance();
   try {
-    // TODO
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +21,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    const types = ['income', 'outcome'];
+
+    if (!types.includes(type)) {
+      throw new Error('Olha seu tipo aí que está errado, blz?');
+    }
+
+    if (value > balance.total && type === 'outcome') {
+      throw new Error('Tá duro maluco, dá não!');
+    }
+
+    const createTransaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return createTransaction;
   }
 }
 
